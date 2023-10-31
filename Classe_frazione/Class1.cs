3c4446425ce0ab0d8cb554620327eb2fe86b34b9
Sciusco.20197 <Sciusco.20197@E2-9-PC18>
2023-10-31 13:53:11 +0100

    inizzializzazione prog

--- Classe_frazione/Class1.cs
+++ Classe_frazione/Class1.cs
@@ -6,9 +6,37 @@ using System.Threading.Tasks;
 
 namespace Classe_frazione
 {
-    internal class Class1
+    internal class Frazione
     {
+        private int numeratore;
+        private int denominatore;
 
-
+        public int Numeratore
+        {
+            get { return numeratore; }
+            set { numeratore = value; }
+        }
+        public int Denominatore
+        {
+            get { return denominatore; }
+            set { denominatore = value; }
+                
+        }
+        public Frazione(int numeratore, int denominatore)
+        {
+            Numeratore= numeratore;
+            Denominatore= denominatore;
+        }
+        public Frazione(Frazione Altrafrazione)
+        {
+            Numeratore = Altrafrazione.Numeratore;
+            Denominatore = Altrafrazione.Denominatore;
+        }
+        public void Semplifica ()
+        {
+            int mcd =MCD(Numeratore,Denominatore);
+            Numeratore /= mcd;
+            Denominatore /= mcd;
+        }
     }
 }
